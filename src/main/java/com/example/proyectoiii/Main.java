201463c4ac17ff9e7de729346c01970267b026d1
dvalencianom <dvalenciano@estudiantec.cr>
2023-05-29 18:07:28 -0600

    Implementación de los sockets (Esta vez si sirve siempre)

--- src/main/java/com/example/proyectoiii/Main.java
+++ src/main/java/com/example/proyectoiii/Main.java
@@ -1,29 +1,73 @@
 package com.example.proyectoiii;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 import static com.example.proyectoiii.Controller.drawGraph;
 import static com.example.proyectoiii.Controller.generateGraph;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+    static BufferedReader in;
+    static PrintWriter out;
+    private Socket client;
+    private boolean done;
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException{
+        client = new Socket("localhost",9999);
+        out = new PrintWriter(client.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        InputHandler inHandler = new InputHandler();
+        Thread t = new Thread(inHandler);
+        t.start();
         Pane root = new Pane();
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Random Graph");
         primaryStage.show();
         generateGraph();
         drawGraph(root);
     }
+    public void shutdown(){
+        done = true;
+        try{
+            out.close();
+            in.close();
+            if(!client.isClosed()){
+                client.close();
+            }
+        }catch (IOException e){
+            //Ignore
+        }
+    }
+    class InputHandler implements Runnable{
 
+        @Override
+        public void run() {
+            try{
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while(!done){
+                    String message = inReader.readLine();
+                    if(message.equals("/quit")){
+                        inReader.close();
+                        shutdown();
+                    }else{
+                        out.write(message);
+                    }
+                }
+            }catch(IOException e){
+                shutdown();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
